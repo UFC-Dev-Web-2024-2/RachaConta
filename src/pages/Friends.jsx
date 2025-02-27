ba19e850116b394850249302595c33d36ef007d2
rodrigosi20 <rodrigosi@alu.ufc.br>
2025-02-27 14:35:52 -0300

    carregar dados

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -7,45 +7,35 @@ import FeedbackModal from '../components/FeedbackModal/FeedbackModal';
 import { AppContext } from '../AppContext';
 
 const Friends = () => {
-    const { state, } = React.useContext(AppContext);
+    const { state, addFriend } = React.useContext(AppContext);
     const [view, setView] = useState('list');
-    const [friends, setFriends] = useState(state.friends);
     const [modalOpen, setModalOpen] = useState(false);
 
-    const handleAddFriendClick = () => {
-        setView('add');
-    };
-
-    const handleAddFriend = (username) => {
-        const friend = {
+    const handleAddFriend = async (username) => {
+        const newFriend = {
             name: username.charAt(0).toUpperCase() + username.slice(1),
             username,
-            avatarBgColor: '#4caf50'
+            avatarBgColor: '#4caf50',
+            userId: state.user.id
         };
-        setFriends([...friends, friend]);
+        await addFriend(newFriend);
         setView('list');
         setModalOpen(true);
     };
 
-    const handleCloseModal = () => {
-        setModalOpen(false);
-    };
-
-    const handleCancelAddFriend = () => {
-        setView('list');
-    };
+    if (state.isLoading) return <div>Carregando amigos...</div>;
 
     return (
         <>
             <Header user={state.user} />
-            <Box sx={{ p: 2, mt: 12 , maxWidth: 600, mx: 'auto' }}>
+            <Box sx={{ p: 2, mt: 12, maxWidth: 600, mx: 'auto' }}>
                 {view === 'list' && (
-                    <FriendsList friends={friends} onAddFriendClick={handleAddFriendClick} />
+                    <FriendsList friends={state.friends} onAddFriendClick={() => setView('add')} />
                 )}
                 {view === 'add' && (
-                    <AddFriend onAdd={handleAddFriend} onCancel={handleCancelAddFriend} />
+                    <AddFriend onAdd={handleAddFriend} onCancel={() => setView('list')} />
                 )}
-                <FeedbackModal open={modalOpen} onClose={handleCloseModal} />
+                <FeedbackModal open={modalOpen} onClose={() => setModalOpen(false)} />
             </Box>
         </>
     );
